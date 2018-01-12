b4e66b8a9e4bf2aad987b54399bed8bdf10de777
chase <chase@ChaseLaptop>
2018-01-12 12:41:03 -0600

    fix for new frclib

--- Robot2018/src/org/usfirst/frc/team2531/robot/RobotMap.java
+++ Robot2018/src/org/usfirst/frc/team2531/robot/RobotMap.java
@@ -1,9 +1,9 @@
 package org.usfirst.frc.team2531.robot;
 
-import frclib.sensors.ADIS16448_IMU;
+import frclib.sensors.ADIS16448;
 import frclib.vision.Vision;
 
 public class RobotMap {
-	public static ADIS16448_IMU imu = new ADIS16448_IMU(); // gyro and accel class
+	public static ADIS16448 imu = new ADIS16448(); // gyro and accel class
 	public static Vision cam; // vision class
 }
