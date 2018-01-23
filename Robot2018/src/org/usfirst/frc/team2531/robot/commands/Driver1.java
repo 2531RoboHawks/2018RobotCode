4d5bef7239ffef681eb6cc46b44da3b84e24cf41
chase <chase@ChaseLaptop>
2018-01-22 21:28:00 -0600

    update

--- Robot2018/src/org/usfirst/frc/team2531/robot/commands/Driver1.java
+++ Robot2018/src/org/usfirst/frc/team2531/robot/commands/Driver1.java
@@ -8,6 +8,16 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class Driver1 extends CommandGroup {
 
 	public Driver1(boolean left) {
-		addSequential(new Turn2Angle(90));
+		addSequential(new TimeDrive(500, 1, 0));
+		if (left) {
+			addSequential(new TimeDrive(500, 1, 1));
+			addSequential(new TimeDrive(500, 0, 1));
+			addSequential(new TimeDrive(500, -1, 1));
+			addSequential(new TimeDrive(500, -1, 0));
+			addSequential(new TimeDrive(500, -1, -1));
+			addSequential(new TimeDrive(500, 0, -1));
+		} else {
+
+		}
 	}
 }
