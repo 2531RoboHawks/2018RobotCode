6220b9714194665ee7e1eff4cd5d16abee47e4a7
chase <chase@ChaseLaptop.lan>
2018-03-11 18:58:58 -0500

    duluth comp code

--- Robot2018/src/org/usfirst/frc/team2531/robot/commands/Driver3.java
+++ Robot2018/src/org/usfirst/frc/team2531/robot/commands/Driver3.java
@@ -15,9 +15,9 @@ public class Driver3 extends CommandGroup {
 				addSequential(new MoveGrabber(true));
 			} else if (data.charAt(0) == 'L') {
 				addSequential(new MoveGrabber(false));
-				addSequential(new TimeDrive(1800, 1, 0));
+				addSequential(new TimeDrive(2000, 1, 0));
 				addSequential(new Turn2Angle(90), 2);
-				addSequential(new TimeDrive(2000, 0.5, 0));
+				addSequential(new TimeDrive(2800, 0.5, 0));
 				addParallel(new MoveArmTo(100, 100));
 				addSequential(new Turn2Angle(90), 2);
 				addSequential(new TimeDrive(1000, 0.5, 0));
