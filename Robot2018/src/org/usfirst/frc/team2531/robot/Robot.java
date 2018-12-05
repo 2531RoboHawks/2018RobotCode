d19620bf4355e4e3f5ea1fa614c00e55cae4df70
chase <chase@DESKTOP-K2FM5KS.isd112.net>
2018-12-05 14:04:24 -0600

    fixed import for vision class

--- Robot2018/src/org/usfirst/frc/team2531/robot/Robot.java
+++ Robot2018/src/org/usfirst/frc/team2531/robot/Robot.java
@@ -14,7 +14,7 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frclib.vision.Vision;
+import frclib.visionold.Vision;
 
 public class Robot extends IterativeRobot {
 
